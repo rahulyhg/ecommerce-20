4984de09fa25a3fadb639fd811343e85e7383470
Renato Freire <ehrenato@gmail.com>
2018-09-21 11:07:00 -0300

    adicionado template do Admin

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
@@ -14,6 +18,13 @@ $app->get('/', function() {
 
 });
 
+$app->get('/admin', function() {
+
+    $page = new Hcode\PageAdmin();
+
+    $page->setTpl("index");
+
+});
+
 $app->run();
 
- ?>
